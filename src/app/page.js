db8eea850cb7130920b4b436dead3c6825f48685
dulkifal <dulkifilibacker>
2024-04-24 16:55:26 +0530

    update evenign

--- src/app/page.js
+++ src/app/page.js
@@ -14,9 +14,21 @@ export default function Home() {
       teacher.push(item.teacher);
     }
   });
+
+  const teachers = {};
+
+  for (const item of data) {
+    const teacherName = item.teacher;
+    if (!teachers[teacherName]) {
+      teachers[teacherName] = [];
+    }
+    teachers[teacherName].push(item);
+  }
+  console.log(teachers);
+
   const teacherList = teacher.map((item) => ({
     value: item,
-    label: item + " Usthad"
+    label: item + " Usthad",
   }));
 
   // filter function
@@ -31,7 +43,7 @@ export default function Home() {
     (acc, item) => acc + item.credit,
     0
   );
-  // total periods 
+  // total periods
   const totalPeriods = filterData([...new Set(data)], searchTerm).reduce(
     (acc, item) => acc + item.period,
     0
@@ -51,6 +63,7 @@ export default function Home() {
           // search
           setSearchTerm(e.value);
         }}
+        
       />
 
       <table
@@ -95,17 +108,48 @@ export default function Home() {
             ) 
             
           )}
-              <tr >
-                
-                <td className="border p-2 text-center ">{}</td>
-                <td className="border p-2 text-center ">{}</td>
-                <td className="border p-2 text-center ">{}</td>
-                <td className="border p-2 text-center ">Total</td>
-                <td className="border p-2 text-center ">{totalCredits}</td>
-                <td className="border p-2 text-center ">{totalPeriods}</td>
-              </tr>
+          <tr>
+            <td className="border p-2 text-center ">{}</td>
+            <td className="border p-2 text-center ">{}</td>
+            <td className="border p-2 text-center ">{}</td>
+            <td className="border p-2 text-center ">Total</td>
+            <td className="border p-2 text-center ">{totalCredits}</td>
+            <td className="border p-2 text-center ">{totalPeriods}</td>
+          </tr>
         </tbody>
       </table>
+      {/* loop through teachers and make table */}
+      <div className="h-50">
+        <h1 className="text-4xl font-bold"> Total Teachers</h1>
+      </div>
+
+      {Object.entries(teachers).map(([teacherName]) => (
+        <table
+          className="w-full text-sm text-left text-gray-500 dark:text-gray-400 mb-4" >
+          <thead>
+            <tr >
+            <th className="border p-2 text-center">No</th>
+              <th className="border p-2 text-center">Teacher</th>
+              <th className="border p-2 text-center">Class</th>
+              <th className="border p-2 text-center">Subject</th>
+              <th className="border p-2 text-center">Credit</th>
+              <th className="border p-2 text-center">Period</th>
+            </tr>
+          </thead>
+          <tbody>
+            {teachers[teacherName].map((item,index) => (
+              <tr key={index}>
+                <td className="border p-2 text-center ">{index}</td>
+                <td className="border p-2 text-center ">{item.class}</td>
+                <td className="border p-2 text-center ">{item.teacher} </td>
+                <td className="border p-2 text-center ">{item.sub}</td>
+                <td className="border p-2 text-center ">{item.credit}</td>
+                <td className="border p-2 text-center ">{item.period}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      ))}
     </main>
   );
 }
